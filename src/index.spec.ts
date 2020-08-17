910a473e8c019c9064e9aa82501141aaeebb62bf
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-17 13:25:31 +0530

    :white_check_mark: Add test run

---
+++ src/index.spec.ts
@@ -0,0 +1,11 @@
+import { join } from "path";
+import { execSync, ExecSyncOptions } from "child_process";
+
+test("test runs", () => {
+  process.env["INPUT_MILLISECONDS"] = "500";
+  const ip = join(__dirname, "..", "lib", "main.js");
+  const options: ExecSyncOptions = {
+    env: process.env,
+  };
+  console.log(execSync(`node ${ip}`, options).toString());
+});
