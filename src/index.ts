69d3c2288ad838e4bf6d70d2c3583a04e29503d8
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-17 19:47:37 +0530

    :recycle: Get octokit instance

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 import { getInput, debug, setFailed, setOutput } from "@actions/core";
+import { getOctokit } from "@actions/github";
+
+const token =
+  getInput("token") || process.env.GH_PAT || process.env.GITHUB_TOKEN;
 
 export const run = async () => {
-  try {
-    const ms: string = getInput("milliseconds");
-    debug(`Waiting ${ms} milliseconds ...`);
+  if (!token) throw new Error("GitHub token not found");
+  const octokit = getOctokit(token);
 
-    debug(new Date().toTimeString());
-    await wait(parseInt(ms, 10));
-    debug(new Date().toTimeString());
+  const ms: string = getInput("milliseconds");
+  debug(`Waiting ${ms} milliseconds ...`);
 
-    setOutput("time", new Date().toTimeString());
-  } catch (error) {
-    setFailed(error.message);
-  }
+  debug(new Date().toTimeString());
+  await wait(parseInt(ms, 10));
+  debug(new Date().toTimeString());
+
+  setOutput("time", new Date().toTimeString());
 };
 
 export const wait = (milliseconds: number) => {
   return new Promise((resolve) => setTimeout(() => resolve(), milliseconds));
 };
+
+run()
+  .then(() => {})
+  .catch((error) => {
+    console.error("ERROR", error);
+    setFailed(error.message);
+  });
